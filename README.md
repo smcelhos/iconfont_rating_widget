838254e8a342de6b00330f22d633e48c10525d1e
smcelhos <stephen.mcelhose@gmail.com>
2014-03-17 15:58:11 -0500

    add click feature to IconFontRater2

--- README.md
+++ README.md
@@ -4,22 +4,28 @@ IconFont Rater
 A simple font based rating [dojo] widget (dijit) using custom [Icomoon] icon fonts.  This widget it keeps it simple.
 You simply set the numStars property to set a scale, empty/half/full properties to a character in your your font-set, or use the defaults provided, and then manipulate the value.
 
+A clickable version was added, with just a few lines of code adding more interactivity, showing that nothing need be lost by using iconfont over say standard images.  The same could be done with hover effect if so desired.
 
-Technology Profile
-===========
-Dojo 
---------
+##Technology Profile
+
+###Dojo 
+
 
 >Javascript toolkit built on top of web standards as it's platform. A toolkit for building high quality desktop and mobile web applications.
 
-Dijit
+###Dijit
 -------
-An extensive set of widgets (user interface components) and the underlying system to support them. Built fully on-top of the Dojo core.
+> An extensive set of widgets (user interface components) and the underlying system to support them. Built fully on-top of the Dojo core.
 
-Icomoon
+### Icomoon
 --------
 > Custom fast, flexible, scalable, vector/resolution independent icons that look great any-size, anywhere.  For those more artsy than myself, it is easy to make your own icons as well.
 
+
+##TODO
+ - ~~Add way to set value with click~~
+ - Build out more interesting custom font
+ - Add keyboard support(up/right increase, down/left decrease value)
 
 [Icomoon]:http://icomoon.io
 [dojo]:http://dojotoolkit.org/
---
+++ js/widgets/IconFontRater2.js
@@ -0,0 +1,103 @@
+﻿define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+	"dojo/on",
+    "dojo/html",
+	"dojo/dom-construct",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin"
+], function (declare, lang, on, html, domConstruct, _WidgetBase, _TemplatedMixin) {
+
+    return declare([_WidgetBase, _TemplatedMixin], {
+        
+        numStars: 5, // scale of rating 0 - numStars
+        empty: "&#xe600", // character to use for empty, custom icomoon font, empty star
+        half: "&#xe601",  // custom icomoon font, half start
+		full: "&#xe602",  // custom icomoon font, full star
+        value: 0,
+        templateString: "<div class='IconFontRater'></div>",
+        buildRendering: function () {
+            // summary: 
+			//		Dijit Lifecycle method.  Build the rendering for this widget.
+			//		Not strictly necessary in IconFontRater as I use a templateString along with _TemplatedMixin
+			//		and the same basic code resides in value setter, but doesn't hurt.
+			
+            var representation = this.buildRepresentation(this.get("value"));
+			
+			// let WidgetBase/TemplatedMixin do their thing
+            this.inherited(arguments);
+
+            html.set(this.domNode, representation);
+
+        },
+        buildRepresentation: function (value) {
+			// summary:
+			//		Build a representation of our widget based on the provided value	\
+			// value: Number
+			//		Numeric value we wish to display.  Negative//zero displayed as zero
+			//		Anything greater than the max rating, is displayed as the max ratting.
+			//	return: HTMLElement
+			//		Internal representation of our widget
+			
+            var representation = domConstruct.create("div");
+            var span = null;
+            for (var i = 0; i < this.numStars; i++, value--) {
+                if (value >= 1) {
+                    span = domConstruct.create("span", {innerHTML:this.full, pos: i});
+                }else if (value > 0 && value < 1) {
+                    span = domConstruct.create("span", {innerHTML:this.half, pos: i});
+                } else {
+                    span = domConstruct.create("span", {innerHTML:this.empty, pos: i});
+                }
+				
+				this.own(on(span, "click", this._onClick.bind(this)));
+				domConstruct.place(span, representation);
+				//representation.append(span);
+				
+			
+                
+            }
+            
+            return representation;
+        },
+       
+	   _onClick: function(evt){
+		   // summary: 
+		   //		onclick handler for each unit in the Rater widget.
+		   //	evt:
+			// 		Normalized mouse click event
+	   var element = evt.srcElement;
+	   var inc = 1;
+	   var offsetWidth = element.offsetWidth;
+		var offsetLeft = element.offsetLeft;
+
+	   if(evt.x < offsetLeft + offsetWidth/2){
+		inc = .5;
+	   }
+	   
+		var elementPos = element.getAttribute('pos');
+		
+		this.set("value", inc + parseInt(elementPos) );
+	
+	   },
+        _setValueAttr: function (value) {
+            // summary:
+			//		Value setter, called whenever a value is set on the widget.
+			//		Client's should use widget.set("value", value) to ensure proper use in all browsers.
+			
+            value = parseFloat(value);
+
+            if (value > this.numStars) { // might need to do special logic to zero out if needed
+                value = this.numStars;
+            }
+            var representation = this.buildRepresentation(value);
+
+            html.set(this.domNode, representation);
+           
+
+            this._set("value", value);
+
+        }
+    });
+
+});
